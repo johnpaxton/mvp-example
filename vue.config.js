c519556c93540f03d77fb3bcc45f08b66558e081
John Paxton <johnpaxton@gmail.com>
2022-03-28 01:12:57 -0400

    Add dotenv-webpack and debugging

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,11 @@ config = {
   transpileDependencies: true,
 };
 
+console.log('env:', process.env);
+
 if (process.env.VUE_APP_ENVIRONMENT === 'azure') {
   console.log('==== Azure ====');
+  const Dotenv = require('dotenv-webpack');
   config.configureWebpack = {
     plugins: [new Dotenv()],
   };
