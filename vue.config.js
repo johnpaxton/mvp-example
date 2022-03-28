dbfe019d31def2087cfca4e5c0da6b091407878d
John Paxton <johnpaxton@gmail.com>
2022-03-28 00:58:02 -0400

    Solve environment issues

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service');
-module.exports = defineConfig({
+
+config = {
   transpileDependencies: true,
-});
+};
+
+if (process.env.VUE_APP_ENVIRONMENT === 'azure') {
+  console.log('==== Azure ====');
+  config.configureWebpack = {
+    plugins: [new Dotenv()],
+  };
+} else {
+  console.log('==== Non-Azure ====');
+}
+
+module.exports = defineConfig(config);
